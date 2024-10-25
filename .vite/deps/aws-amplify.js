3b71a0b491317fcf1f848a4e507154f0e3208c13
sri varadarajan <browngeek@gmail.com>
2024-10-25 16:56:13 -0700

    the notes app

---
+++ .vite/deps/aws-amplify.js
@@ -0,0 +1,10 @@
+import {
+  DefaultAmplify
+} from "./chunk-N4DQG4DP.js";
+import "./chunk-5YXTIY23.js";
+import "./chunk-DSIIKXPB.js";
+import "./chunk-G3PMV62Z.js";
+export {
+  DefaultAmplify as Amplify
+};
+//# sourceMappingURL=aws-amplify.js.map
---
+++ .vite/deps/aws-amplify_storage.js
@@ -0,0 +1,26 @@
+import {
+  StorageError,
+  copy,
+  downloadData,
+  getProperties,
+  getUrl,
+  isCancelError,
+  list,
+  remove,
+  uploadData
+} from "./chunk-C7Q2ENK3.js";
+import "./chunk-5YXTIY23.js";
+import "./chunk-DSIIKXPB.js";
+import "./chunk-G3PMV62Z.js";
+export {
+  StorageError,
+  copy,
+  downloadData,
+  getProperties,
+  getUrl,
+  isCancelError,
+  list,
+  remove,
+  uploadData
+};
+//# sourceMappingURL=aws-amplify_storage.js.map
